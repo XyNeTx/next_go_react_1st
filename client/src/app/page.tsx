f32a330b73cc7da0306b05115cf8f5d0d965bac2
Sitthiporn Polmart <sitthiporn_p@hinothailand.com>
2025-07-03 11:21:17 +0700

    change location for package.json to run easier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
+function Title() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,6 +20,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Title />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
